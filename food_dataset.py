476e7d03569485978c9af702825977ca24a15672
Tamas Ujhelyi <[200~baloghghadi@gmail.com>
2025-04-14 17:41:00 +0200

    dataset implemented

---
+++ food_dataset.py
@@ -0,0 +1,38 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+
+class FoodDataset(Dataset):
+    def __init__(self, root_dir, classes, labels, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.labels = labels
+        self.classes = classes
+        self.class_to_idx = {clsname: i for i, clsname in enumerate(classes)}
+        self.img_paths = []
+        for cls_name in classes:
+            cls_dir = os.path.join(root_dir, cls_name)
+            if os.path.isdir(cls_dir):
+                for img_name in os.listdir(cls_dir):
+                    img_path = os.path.join(cls_dir, img_name)
+                    if os.path.isfile(img_path):
+                        self.img_paths.append((img_path, self.class_to_idx[cls_name]))
+        
+    def __len__(self):
+        return len(self.img_paths)
+
+    def __getitem__(self, idx):
+        img_path, label = self.img_paths[idx]
+        img = Image.open(img_path).convert('RGB')
+        if self.transform:
+            img = self.transform(img)
+        return img, label
+    
+    def get_idx(self, class_name):
+        return self.class_to_idx[class_name]
+    
+    def get_class_label(self, idx):
+        return self.labels[idx]
+    
+    def get_class_name(self, idx):
+        return self.classes[idx]
