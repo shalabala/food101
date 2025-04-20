ad099aff5a1095cebc7d3359c83373d086ae82c9
Tamas Ujhelyi <[200~baloghghadi@gmail.com>
2025-04-20 20:15:38 +0200

    model fix

--- food_model.py
+++ food_model.py
@@ -30,7 +30,7 @@ class FoodModel(nn.Module):
         x = F.relu(self.bn4(self.conv4(x)))
         x = F.max_pool2d(x, 2)
         x = x.view(x.size(0), -1)
-        # x = F.relu(self.bn5(self.fc1(x)))
+        x = F.relu(self.fc1(x))
         # x = F.relu(self.fc2(x))
         x = self.fc3(x)
 
